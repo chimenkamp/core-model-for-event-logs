1a2356abfc6177e2244c083bd8de244032c53b36
chimenkamp <chimenkamp@gmail.com>
2024-07-19 07:04:13 +0200

    refactor schema to class level

--- src/classes_.py
+++ src/classes_.py
@@ -1,37 +1,47 @@
-from typing import List, Optional, Union, Literal
+from abc import ABC, abstractmethod
+from typing import List, Optional, Union, Literal, Any
 import datetime
 import json
 import pandas as pd
 from graphviz import Digraph
 
-from src.exporter.json_schema import define_schema
+
+class Serializable(ABC):
+    """
+    Abstract base class to enforce serialization implementation.
+    """
+
+    @abstractmethod
+    def serialize(self) -> dict:
+        pass
 
 
-class Attribute:
+class Attribute(Serializable):
     """
-    Class to represent an attribute of an object or event
+    Class to represent an attribute of an object or event.
     """
+
     def __init__(self, key: str, value: Union[str, float, int]) -> None:
-        """
-        :param key: The key of the attribute
-        :param value: The value of the attribute
-        """
         self.key = key
         self.value = value
 
     def __repr__(self) -> str:
         return f"Attribute(key={self.key}, value={self.value})"
 
+    def serialize(self) -> dict:
+        return {
+            "key": self.key,
+            "value": self.value
+        }
 
-class Event:
+
+class Event(Serializable):
     """
-    Class to represent an event
+    Class to represent an event.
     """
-    def __init__(self,
-                 event_id: str,
-                 event_type: Literal["process event", "iot event"],
-                 attributes: List[Attribute],
-                 data_source: Optional['DataSource'] = None) -> None:
+
+    def __init__(self, event_id: str, event_type: Literal["process event", "iot event"],
+                 attributes: List[Attribute], data_source: Optional['DataSource'] = None) -> None:
         self.event_id = event_id
         self.event_type = event_type
         self.attributes: List[Attribute] = attributes
@@ -41,119 +51,111 @@ class Event:
 
     def __repr__(self) -> str:
         return (f"Event(id={self.event_id}, type={self.event_type}, attributes={self.attributes}, "
-                f"object_ids={[o.object_id for o in self.related_objects]}, data_source_id={self.data_source.source_id  if self.data_source else None}")
+                f"object_ids={[o.object_id for o in self.related_objects]}, data_source_id={self.data_source.source_id if self.data_source else None})")
 
     def add_data_source(self, data_source: 'DataSource') -> None:
-        """
-        Add a data source to the event
-        :param data_source:  Reference to the data source
-        :return:
-        """
         self.data_source = data_source
         data_source.record_event(self)
+
+    def serialize(self) -> dict:
+        return {
+            "event_id": self.event_id,
+            "event_type": self.event_type,
+            "attributes": [attr.serialize() for attr in self.attributes],
+            "timestamp": self.timestamp.isoformat(),
+            "data_source": self.data_source.serialize() if self.data_source else None,
+            "related_objects": [obj.object_id for obj in self.related_objects]
+        }
 
 
 class IoTEvent(Event):
     """
-    Class to represent an IoT event
+    Class to represent an IoT event.
     """
-    def __init__(self, event_id: str, attributes: List[Attribute],
-                 data_source: Optional['DataSource'] = None) -> None:
-        """
-        :param event_id: The unique identifier of the event
-        :param attributes: The attributes of the event
-        :param data_source: Reference to the data source
-        """
+
+    def __init__(self, event_id: str, attributes: List[Attribute], data_source: Optional['DataSource'] = None) -> None:
         super().__init__(event_id, "iot event", attributes, data_source)
 
 
-class Activity:
+class Activity(Serializable):
     """
-    Class to represent an activity
+    Class to represent an activity.
     """
+
     def __init__(self, activity_id: str, activity_type: str) -> None:
-        """
-        :param activity_id: The unique identifier of the activity
-        :param activity_type: The type of the activity
-        """
         self.activity_id: str = activity_id
         self.activity_type: str = activity_type
+
+    def serialize(self) -> dict:
+        return {
+            "activity_id": self.activity_id,
+            "activity_type": self.activity_type
+        }
 
 
 class ProcessEvent(Event):
     """
-    Class to represent a process event
+    Class to represent a process event.
     """
-    def __init__(self, event_id: str, attributes: List[Attribute],
-                 data_source: Optional['DataSource'] = None) -> None:
-        """
-        :param event_id: The unique identifier of the event
-        :param attributes: The attributes of the event
-        :param data_source: Reference to the data source
-        """
+
+    def __init__(self, event_id: str, attributes: List[Attribute], data_source: Optional['DataSource'] = None) -> None:
         super().__init__(event_id, "process event", attributes, data_source)
         self.activities: List[Activity] = []
 
     def add_activity(self, activity: Activity) -> None:
-        """
-        Add an activity to the process event
-        :param activity: The activity to add
-        :return:
-        """
         self.activities.append(activity)
 
+    def serialize(self) -> dict:
+        data = super().serialize()
+        data["activities"] = [activity.serialize() for activity in self.activities]
+        return data
 
-class DataSource:
+
+class DataSource(Serializable):
     """
-    Class to represent a data source
+    Class to represent a data source.
     """
+
     def __init__(self, source_id: str, source_type: str) -> None:
-        """
-        :param source_id: The unique identifier of the data source
-        :param source_type: The type of the data source
-        """
         self.source_id = source_id
         self.source_type = source_type
         self.events: List[Event] = []
 
     def record_event(self, event: Event) -> None:
-        """
-        Record an event in the data source
-        :param event: Event to record
-        :return:
-        """
         event.data_source = self
         self.events.append(event)
 
     def __repr__(self) -> str:
         return f"DataSource(id={self.source_id}, type={self.source_type}, events={self.events})"
 
+    def serialize(self) -> dict:
+        return {
+            "source_id": self.source_id,
+            "source_type": self.source_type,
+            "events": [event.event_id for event in self.events]
+        }
+
 
 class IS(DataSource):
     """
-    Class to represent an information system
+    Class to represent an information system.
     """
+
     def __init__(self, system_id: str) -> None:
-        """
-        :param system_id: The unique identifier of the information system
-        """
         super().__init__(system_id, "information system")
 
 
 class SOSA:
     """
-    Class to represent the SOSA ontology / Namespace
+    Class to represent the SOSA ontology / Namespace.
     """
-    class Observation:
+
+    class Observation(Serializable):
         """
-        Class to represent an observation
+        Class to represent an observation.
         """
+
         def __init__(self, observation_id: str, observed_property: str, value: Union[str, float, int]) -> None:
-            """
-            :param observation_id: The unique identifier of the observation
-            :param observed_property: The property being observed
-            :param value: The value of the observation
-            """
             self.observation_id = observation_id
             self.observed_property = observed_property
             self.value = value
@@ -161,36 +163,37 @@ class SOSA:
         def __repr__(self) -> str:
             return f"Observation(id={self.observation_id}, property={self.observed_property}, value={self.value})"
 
+        def serialize(self) -> dict:
+            return {
+                "observation_id": self.observation_id,
+                "observed_property": self.observed_property,
+                "value": self.value
+            }
+
     class IoTDevice(DataSource):
         """
-        Class to represent an IoT device
+        Class to represent an IoT device.
         """
+
         def __init__(self, device_id: str) -> None:
-            """
-            :param device_id: The unique identifier of the IoT device
-            """
             super().__init__(device_id, "iot device")
             self.observations: List[SOSA.Observation] = []
 
         def add_observation(self, observation: 'SOSA.Observation') -> None:
-            """
-            Add an observation to the IoT device
-            :param observation: The observation to add
-            :return:
-            """
             self.observations.append(observation)
 
+        def serialize(self) -> dict:
+            data = super().serialize()
+            data["observations"] = [obs.serialize() for obs in self.observations]
+            return data
 
-class Object:
+
+class Object(Serializable):
     """
-    Class to represent an object
+    Class to represent an object.
     """
+
     def __init__(self, object_id: str, object_type: str, attributes: List[Attribute]) -> None:
-        """
-        :param object_id: The unique identifier of the object
-        :param object_type: The type of the object
-        :param attributes: The attributes of the object
-        """
         self.object_id = object_id
         self.object_type = object_type
         self.attributes = attributes
@@ -199,39 +202,35 @@ class Object:
         self.data_sources: List[DataSource] = []
 
     def add_event(self, event: Event) -> None:
-        """
-        Add an event to the object
-        :param event: The event to add
-        :return:
-        """
         event.related_objects.append(self)
         self.events.append(event)
 
     def add_related_object(self, related_object: 'Object') -> None:
-        """
-        Add a related object to the object
-        :param related_object: The related object to add
-        :return:
-        """
         self.related_objects.append(related_object)
 
     def add_data_source(self, data_source: DataSource) -> None:
-        """
-        Add a data source to the object
-        :param data_source: Reference to the data source
-        :return:
-        """
         self.data_sources.append(data_source)
 
     def __repr__(self) -> str:
-        return (f"Object(id={self.object_id}, type={self.object_type}, attributes={self.attributes}, "
-                f"events={self.events})")
+        return (
+            f"Object(id={self.object_id}, type={self.object_type}, attributes={self.attributes}, events={self.events})")
+
+    def serialize(self) -> dict:
+        return {
+            "object_id": self.object_id,
+            "object_type": self.object_type,
+            "attributes": [attr.serialize() for attr in self.attributes],
+            "related_objects": [obj.object_id for obj in self.related_objects],
+            "events": [event.event_id for event in self.events],
+            "data_sources": [ds.source_id for ds in self.data_sources]
+        }
 
 
-class CCM:
+class CCM(Serializable):
     """
-    Class to represent a Common-Core Model (CCM) dataset
+    Class to represent a Common-Core Model (CCM) dataset.
     """
+
     def __init__(self) -> None:
         self.event_log: List[Event] = []
         self.objects: List[Object] = []
@@ -267,7 +266,7 @@ class CCM:
             related_objects = [obj for obj in self.objects if obj in event.related_objects]
 
             for related_object in related_objects:
-                row = {
+                row: dict[str, Any] = {
                     'ccm:event_id': event.event_id,
                     'ccm:event_type': event.event_type,
                     'ccm:timestamp': event.timestamp,
@@ -277,15 +276,12 @@ class CCM:
                     'ccm:data_source_type': event.data_source.source_type if event.data_source else None
                 }
 
-                # Add event attributes
                 for attr in event.attributes:
                     row[f'event:{attr.key}'] = attr.value
 
-                # Add object attributes
                 for attr in related_object.attributes:
                     row[f'object:{attr.key}'] = attr.value
 
-                # Add data source attributes if available
                 if event.data_source:
                     for ds in self.data_sources:
                         if ds.source_id == event.data_source.source_id:
@@ -294,7 +290,6 @@ class CCM:
                                     for e_attr in attr.attributes:
                                         row[f'data_source_event:{e_attr.key}'] = e_attr.value
 
-                # Add activities if the event is a ProcessEvent
                 if isinstance(event, ProcessEvent):
                     for activity in event.activities:
                         row[f'activity:{activity.activity_id}_type'] = activity.activity_type
@@ -305,7 +300,7 @@ class CCM:
         return df
 
     def save_to_json(self, file_path: str) -> None:
-        data = define_schema(self, IS)
+        data = self.serialize()
 
         with open(file_path, 'w') as file:
             json.dump(data, file, indent=4)
@@ -314,11 +309,20 @@ class CCM:
         return (f"CCM(event_log={self.event_log}, objects={self.objects}, data_sources={self.data_sources}, "
                 f"information_systems={self.information_systems}, iot_devices={self.iot_devices})")
 
+    def serialize(self) -> dict:
+        return {
+            "ccm:events": [event.serialize() for event in self.event_log],
+            "ccm:objects": [obj.serialize() for obj in self.objects],
+            # "ccm:data_sources": [ds.serialize() for ds in self.data_sources],
+            "ccm:information_systems": [is_system.serialize() for is_system in self.information_systems],
+            "ccm:iot_devices": [iot_device.serialize() for iot_device in self.iot_devices],
+            "ccm:activities": [activity.serialize() for activity in self.activities]
+        }
+
     def visualize(self, output_file: str) -> None:
         dot = Digraph(comment='CCM Diagram')
         edges = set()
 
-        # Add objects
         for obj in self.objects:
             dot.node(obj.object_id, label=f"Object\n{obj.object_type}\n{obj.object_id}", shape='box', style='filled',
                      color='lightyellow')
@@ -332,14 +336,12 @@ class CCM:
                     dot.edge(*edge)
                     edges.add(edge)
 
-            # Add related objects
             for related_obj in obj.related_objects:
                 edge = (obj.object_id, related_obj.object_id)
                 if edge not in edges:
                     dot.edge(*edge)
                     edges.add(edge)
 
-        # Add events
         for event in self.event_log:
             event_label = "Process Event" if event.event_type == "process event" else "IoT Event"
             dot.node(event.event_id, label=f"{event_label}\n{event.event_id}", shape='box', style='filled',
@@ -360,7 +362,6 @@ class CCM:
                     dot.edge(*edge)
                     edges.add(edge)
 
-            # Add activities if event is a ProcessEvent
             if isinstance(event, ProcessEvent):
                 for activity in event.activities:
                     dot.node(activity.activity_id, label=f"Activity\n{activity.activity_type}\n{activity.activity_id}",
@@ -370,7 +371,6 @@ class CCM:
                         dot.edge(*edge)
                         edges.add(edge)
 
-        # Add data sources
         for ds in self.data_sources:
             dot.node(ds.source_id, label=f"Data Source\n{ds.source_type}\n{ds.source_id}", shape='box', style='filled',
                      color='lightpink')
@@ -381,7 +381,6 @@ class CCM:
                     dot.edge(*edge)
                     edges.add(edge)
 
-        # Add IoT Devices and Observations
         for device in self.iot_devices:
             dot.node(device.source_id, label=f"IoT Device\n{device.source_id}", shape='box', style='filled',
                      color='lightcyan')
@@ -401,7 +400,6 @@ class CCM:
                     dot.edge(*edge)
                     edges.add(edge)
 
-        # Add Information Systems
         for is_system in self.information_systems:
             dot.node(is_system.source_id, label=f"Information System\n{is_system.source_id}", shape='box',
                      style='filled', color='black', fontcolor='white')
@@ -414,7 +412,6 @@ class CCM:
                         dot.edge(*edge)
                         edges.add(edge)
 
-        # Add relation between objects and data sources
         for obj in self.objects:
             for ds in obj.data_sources:
                 edge = (obj.object_id, ds.source_id)
@@ -422,7 +419,5 @@ class CCM:
                     dot.edge(*edge)
                     edges.add(edge)
 
-        # dot.render(output_file, view=True)
         dot.format = 'png'
-        # dot.save(output_file)
         dot.render(output_file, view=True)
